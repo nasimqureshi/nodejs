af9d9e3e0d37197a7a83b299388c2a4eb72c6313
nasimqureshi <qureshi.nasim@yahoo.com>
2023-05-29 22:47:58 -0500

    commit

--- app.js
+++
@@ -1,6 +0,0 @@
-const a = require('./index')
-
-
-a.average( 10, 30);
-
-a.percent(20,100);
--- index.js
+++
@@ -1,9 +0,0 @@
-const a = {
-    average: (a, b) => {
-        console.log(a + b) / 2;
-    },
-    percent: (a, b) => {
-        console.log((a / b) * 100)
-    }
-}
-module.exports = a;
